458fd0b4e2f5e41524588a67838eeede12b213f6
shossainn <73153725+shossainn@users.noreply.github.com>
2023-01-21 22:20:54 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,115 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/"
+];
+
+let passEl = document.getElementById("pass-el");
+let genBtn = document.getElementById("gen-btn");
+
+// passEl.textContent = "Your Random Password is: " + genBtn;
+
+passwordLengthOne = 12;
+passwordLengthTwo = 16;
+passwordLengthThree = 32;
+
+function generate() {
+  let randomPass = Math.floor(Math.random() * characters.length);
+  return characters[randomPass];
+}
+
+function generatePassOne() {
+  let rando = "";
+  for (let i = 0; i < passwordLengthOne; i++) {
+    passEl.textContent = rando += generate();
+  }
+  return rando;
+}
